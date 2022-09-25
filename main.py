8ddff716e13ea14c33586d1273b2696f5c90006c
MrinmoyNotFound <108539530+MrinmoyNotFound@users.noreply.github.com>
2022-09-25 17:13:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import random
 
 choices = ["rock","paper","scissor"]
 com_choice = random.choice(choices)
+
 while True:
   user_choice = str(input("Choice Your Choice\nRock, Paper and Scissor : ")).lower()
 #if user input is invalid or same
   if com_choice == user_choice:
-	  print(msg.tie_msg,f"Computer choice {com_choice}")
+	  print(msg.tie_msg,f"\nComputer choice {com_choice}")
   elif user_choice not in choices:
-	  print(msg.invalid_input,f"Computer choice : {com_choice}")
+	  print(msg.invalid_input)
 #win function
   elif user_choice == "paper":
 	  if com_choice == "rock":
@@ -31,9 +32,10 @@ while True:
     if com_choice == "scissor":
 	    print(msg.lose_msg,f"Computer choice : {com_choice}")
   play_again = str(input("Do you want to play again? Type yes/no ")).lower()
-
-
-  if play_again == 'yes':
+  play_dic = ("yes","no")
+  if play_again not in play_dic:
+    print("It's seem to be that your entered command is invalid,anyway play again ;)")
+  elif play_again == 'yes':
     print("Ok") 
   else:
     play_again == 'no'
